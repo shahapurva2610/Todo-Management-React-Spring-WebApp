59431079fe95d131f5263014c1db7dfb5230a5a4
Shahapurva <shahapurva2610@yahoo.com>
2021-06-03 23:51:05 -0700

    Todo Application

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'alex', 'Cloud Computing', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'alex', 'Machine Learning', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'alex', 'Blockchain', sysdate(), false);
